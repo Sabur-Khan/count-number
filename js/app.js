356723c507350be22fbd34d59e06318e7b850147
Noob-developer-sobur <soburkhan210@gmail.com>
2023-07-24 13:42:45 +0600

    push new code

--- js/app.js
+++ js/app.js
@@ -1,27 +1,39 @@
-let counter = document.querySelector('.counter')
-let count = 0;
+let counter = document.querySelectorAll('.counter');
 
-function incremant(){
-    count++;
-    counter.innerHTML = "Happy client: " + count;
-    
-    // ai jaigai buji nai
-    if(counter.dataset.number == count){
-     clearInterval(stop)   
+let arr = Array.from(counter);
+
+arr.map((startCount) =>{
+
+    let counting = 0;
+
+    function incremant(){
+        counting++
+
+        startCount.innerHTML = counting;
+
+        if(counting == startCount.dataset.number){
+            clearInterval(stop)
+        }
     }
-}
-let stop = setInterval(incremant,100)
-
-// next counter
-let counters = document.querySelector('.next')
-let runs = 0;
-function startingPoint(){
-    runs++;
-    counters.innerHTML = "Our projects: " + runs;
-    if(counters.dataset.numbers == runs){
-        clearInterval(stops)  
-    }
-}
-let stops = setInterval(startingPoint,100)
+    let stop = setInterval(incremant, startCount.dataset.speed)
+})
 
 
+
+
+// arr.map((sobur)=>{
+//     console.log(sobur)
+// })
+
+// let myArr = Array.from(counter);
+// myArr.map((countItem)=>{
+//     let count = 0;
+//     function incremant() {
+//         count++
+//         countItem.innerHTML = count;
+//         if(count == countItem.dataset.number){
+//             clearInterval(stop)
+//         }
+//     }
+//     let stop = setInterval(incremant, countItem.dataset.speed)
+// })
